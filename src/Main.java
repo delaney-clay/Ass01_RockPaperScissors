f4d80dd0844659527ddd35a6c4efac12bc09e4d5
delaney-clay <claydk@mail.uc.edu>
2026-06-26 19:39:04 -0400

    before first test run commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static com.sun.xml.internal.bind.v2.schemagen.Util.equalsIgnoreCase;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,6 +10,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String playAChoice = "";
         String playBChoice = "";
+        String continue = "";
         boolean playADone = false;
         boolean playBDone = false;
         boolean done = false;
@@ -44,8 +47,69 @@ public class Main {
                     playBDone = false;
                 }
             }while(!playBDone);
+            if(playAChoice.equalsIgnoreCase("r"))
+            {
+                if(playBChoice.equalsIgnoreCase("s"))
+                {
+                    System.out.println("Rock breaks Scissors, Player A wins!");
+                }
+                else if(playBChoice.equalsIgnoreCase("p"))
+                {
+                    System.out.println("Paper covers Rock, Player B wins!");
+                }
+                else
+                {
+                    System.out.println("It's a tie!");
+                }
 
-
-        }
+            }
+            else if(playAChoice.equalsIgnoreCase("p"))
+            {
+                if(playBChoice.equalsIgnoreCase("r"))
+                {
+                    System.out.println("Paper covers Rock, Player A wins!");
+                }
+                else if(playBChoice.equalsIgnoreCase("s"))
+                {
+                    System.out.println("Scissors cut Paper, Player B wins!");
+                }
+                else
+                {
+                    System.out.println("It's a tie!");
+                }
+            }
+            else
+            {
+                if(playBChoice.equalsIgnoreCase("p"))
+                {
+                    System.out.println("Scissors cut Paper, Player A wins!");
+                }
+                else if(playBChoice.equalsIgnoreCase("r"))
+                {
+                    System.out.println("Rock breaks Scissors, Player B wins!");
+                }
+                else
+                {
+                    System.out.println("It's a tie!");
+                }
+            }
+            do
+            {
+                System.out.print("Do you want to play again? (Y/N): ");
+                continue = in.nextLine()
+                if(continue.equalsIgnoreCase("y"))
+                {
+                    done = false;
+                }
+                else if(continue.equalsIgnoreCase("n"))
+                {
+                    done = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input. Please try again.");
+                }
+            }
+        }while(!done);
     }
 }
