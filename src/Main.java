807d5cd4d429785ff0f9babab4877c11cc89afa1
delaney-clay <claydk@mail.uc.edu>
2026-06-26 19:21:53 -0400

    player input committ

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String playAChoice = "";
+        String playBChoice = "";
+        boolean playADone = false;
+        boolean playBDone = false;
+        boolean done = false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do
+        {
+            do
+            {
+                System.out.println("Player A, enter your move:\nR for Rock\nP for Paper\nS fpr Scissors ");
+                playAChoice = in.nextLine();
+
+                if(playAChoice.equalsIgnoreCase("r") || playAChoice.equalsIgnoreCase("p") || playAChoice.equalsIgnoreCase("s"))
+                {
+                    playADone = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input. Please try again.");
+                    playADone = false;
+                }
+            }while(!playADone);
+            do
+            {
+                System.out.println("Player B, enter your move:\nR for Rock\nP for Paper\nS fpr Scissors ");
+                playBChoice = in.nextLine();
+
+                if(playBChoice.equalsIgnoreCase("r") || playBChoice.equalsIgnoreCase("p") || playBChoice.equalsIgnoreCase("s"))
+                {
+                    playBDone = true;
+                }
+                else
+                {
+                    System.out.println("Invalid input. Please try again.");
+                    playBDone = false;
+                }
+            }while(!playBDone);
+
+
         }
     }
 }
